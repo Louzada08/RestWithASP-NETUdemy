df9080d374d15348e8df01e66e56204c9a32481f
louzada08 <valuz.anderson.to@gmail.com>
2019-09-19 21:58:32 -0300

    Implementing all principal verbs

---
+++ RestWithASPNETUdemy 01 - Using- Diferents Verbs/RestWithASPNETUdemy/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Models
+{
+    //[Table("persons")]
+    public class Person
+    {
+        //[Column("FirstName")]
+        public string FirstName { get; set; }
+
+        //[Column("LastName")]
+        public string LastName { get; set; }
+
+        //[Column("Address")]
+        public string Address { get; set; }
+
+        //[Column("Gender")]
+        public string Gender { get; set; }
+    }
+}
